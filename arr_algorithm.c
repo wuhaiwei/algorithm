f3e47e813c1dc910e629f6e9bce0d960e938006e
吴海伟 <wuhaiwei@qding.me>
2020-09-02 16:34:00 +0800

    合并两个有序链表、合并两个有序数组

--- arr_algorithm.c
+++ arr_algorithm.c
@@ -142,7 +142,7 @@ void HeapLittleAdjust(int array[], int i, int nLength)
     // 最后把需要调整的元素值放到合适的位置
     array[i] = nTemp;
 }
-//最小的K个数
+//数组中最小的K个数
 int* getLeastNumbers(int array[], int length, int k, int* returnSize){
 	 int *resultArr = (int *)malloc(k * sizeof(int));
 
@@ -163,6 +163,24 @@ int* getLeastNumbers(int array[], int length, int k, int* returnSize){
     }
     return resultArr;
 }
+
+
+
+
+
+//给你两个有序整数数组 nums1 和 nums2，请你将 nums2 合并到 nums1 中，使 nums1 成为一个有序数组。
+//假定nums1有足够的空间
+void merge(int* nums1, int nums1Size, int m, int* nums2, int nums2Size, int n){
+    int tail = m + n - 1;
+    --m, --n;
+    while (m >= 0 && n >= 0) {
+        if (nums1[m] > nums2[n]) nums1[tail--] = nums1[m--];
+        else                     nums1[tail--] = nums2[n--];
+    }
+    while (n >= 0)               nums1[tail--] = nums2[n--];
+}
+
+
 
 
 int main(){
--- link_algorithm.c
+++ link_algorithm.c
@@ -64,6 +64,39 @@ struct ListNode *getIntersectionNode(struct ListNode *headA, struct ListNode *he
 }
 
 
+//合并两个有序链表
+struct ListNode* mergeTwoLists(struct ListNode* l1, struct ListNode* l2){
+    if(!l1) return l2;
+    if(!l2) return l1;
+    struct ListNode *head = (struct ListNode *)malloc(sizeof(struct ListNode)), *t = head;
+    while(l1 && l2){
+        if(l1->val <= l2->val){
+            t->next = l1;
+            l1 = l1->next;
+        }else{
+            t->next = l2;
+            l2 = l2->next;
+        }
+        t = t->next;
+    }
+    if(l1) t->next = l1;
+    else if(l2) t->next = l2;
+    return head->next;
+}
+
+
+//递归法合并两个有序链表
+struct ListNode* mergeTwoListsByRecursion(struct ListNode* l1, struct ListNode* l2){
+    if(!l1) return l2;
+    if(!l2) return l1;
+    if(l1->val <= l2->val){
+        l1->next = mergeTwoListsByRecursion(l1->next,l2);
+        return l1;
+    }else{
+        l2->next = mergeTwoListsByRecursion(l1,l2->next);
+        return l2;
+    }
+}
 
 
 int main(){
